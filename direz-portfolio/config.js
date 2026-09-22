63450e61d357c776849fa4d00e32a337791af9a6
Dio213z <diod86711@gmail.com>
2026-09-22 18:14:17 +0700

    Delete direz-portfolio directory

--- direz-portfolio/config.js
+++
@@ -1,6 +0,0 @@
-// Isi dari Supabase Project Settings > API. Hanya public anon/publishable key.
-// JANGAN masukkan service_role key atau password admin ke file ini.
-window.DIREZ_CONFIG = {
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: ''
-};
